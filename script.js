90dfd6563285375e92b01ab5ef90757f44e50b1e
MisbahNayab12 <160389499+MisbahNayab12@users.noreply.github.com>
2024-09-28 20:12:32 +0500

    Created Dynamic Resume.

---
+++ script.js
@@ -0,0 +1,22 @@
+var form = document.getElementById('formTemp');
+var generatedElement = document.getElementById('generatedResume');
+form.addEventListener('submit', function (event) {
+    event.preventDefault();
+    var firstName = document.getElementById('FN').value;
+    var lastName = document.getElementById('LN').value;
+    var email = document.getElementById('EI').value;
+    var birthdate = document.getElementById('DOB').value;
+    var gender = document.getElementById('gender').value;
+    var qualification = document.getElementById('qualification').value;
+    var grade = document.getElementById('grade').value;
+    var year = document.getElementById('passingYear').value;
+    var exp = document.getElementById('Exp').value;
+    var skills = document.getElementById('skills').value;
+    var resume = "\n    <h2><b>Resume</b></h2>\n    <h3>Personal Information</h3>\n    <p><b> First Name:</b>".concat(firstName, "</p>\n    <p><b> Last Name:</b>").concat(lastName, "</p>\n    <p><b> Email I.D:</b>").concat(email, "</p>\n    <p><b> Date of Birth:</b>").concat(birthdate, "</p>\n    <p><b> Gender:</b>").concat(gender, "</p>\n\n    <h3><b>Education</b></h3>\n    <p>").concat(qualification, "</p>\n    <p>").concat(grade, "</p>\n    <p>").concat(year, "</p>\n\n    <h3><b>Experience</b></h3>\n    <p>").concat(exp, "</p>\n\n    <h3><b>Skills</b></h3>\n    <p>").concat(skills, "</p>\n    ");
+    if (generatedElement) {
+        generatedElement.innerHTML = resume;
+    }
+    else {
+        console.error("Some elements are missing");
+    }
+});
---
+++ script.ts
@@ -0,0 +1,44 @@
+const form = document.getElementById('formTemp') as HTMLFormElement;
+const generatedElement = document.getElementById('generatedResume') as HTMLDivElement;
+
+form.addEventListener('submit', (event: Event)=> {
+    event.preventDefault();
+
+    const firstName = (document.getElementById('FN') as HTMLInputElement).value;
+    const lastName = (document.getElementById('LN') as HTMLInputElement).value;
+    const email = (document.getElementById('EI') as HTMLInputElement).value;
+    const birthdate = (document.getElementById('DOB') as HTMLInputElement).value;
+    const gender = (document.getElementById('gender') as HTMLSelectElement).value;
+    const qualification = (document.getElementById('qualification') as HTMLSelectElement).value;
+    const grade = (document.getElementById('grade') as HTMLSelectElement).value;
+    const year = (document.getElementById('passingYear') as HTMLSelectElement).value;
+    const exp = (document.getElementById('Exp') as HTMLInputElement).value;
+    const skills = (document.getElementById('skills') as HTMLInputElement).value;
+
+    const resume = `
+    <h2><b>Resume</b></h2>
+    <h3>Personal Information</h3>
+    <p><b> First Name:</b>${firstName}</p>
+    <p><b> Last Name:</b>${lastName}</p>
+    <p><b> Email I.D:</b>${email}</p>
+    <p><b> Date of Birth:</b>${birthdate}</p>
+    <p><b> Gender:</b>${gender}</p>
+
+    <h3><b>Education</b></h3>
+    <p>${qualification}</p>
+    <p>${grade}</p>
+    <p>${year}</p>
+
+    <h3><b>Experience</b></h3>
+    <p>${exp}</p>
+
+    <h3><b>Skills</b></h3>
+    <p>${skills}</p>
+    `;
+
+    if (generatedElement){
+        generatedElement.innerHTML = resume;
+    } else {
+        console.error("Some elements are missing");
+    }
+});
